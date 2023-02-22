4a0ec428f92b69d410e91cf1ea9edb628ad56045
floitex <mursalbekov.merey@gmail.com>
2023-02-22 21:52:48 +0600

    qwerty

---
+++ lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent {
+  products = [...products];
+
+  share(product: Product) {
+    window.alert('The product has been shared!');
+    window.open(`https://api.whatsapp.com/send?phone=77767283492&text=${product.link}`);
+  }
+
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
